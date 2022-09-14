cf6a6a2130738e89f06a1082f636214f03a5bfd1
rohith42 <36945963+rohith42@users.noreply.github.com>
2022-09-13 19:21:15 -0700

    added CRUD functions to context

--- src/store/roster-context.js
+++ src/store/roster-context.js
@@ -3,13 +3,23 @@ import { createContext, useState } from "react";
 const RosterContext = createContext();
 
 export function RosterProvider({ children }) {
+    const [roster, setRoster] = useState([]);
+    const [selected, setSelected] = useState("");
+    
+    function addToRoster(newMember) {
+        setRoster((prevRoster) => [...prevRoster, newMember]);
+    }
+
+    function deleteFromRoster(toDelete) {
+        setRoster((prevRoster) => prevRoster.filter(player => player !== toDelete));
+    }
+
+    function updateSelected(newSelection) {
+        setSelected(newSelection);
+    }
+    
     return (
-        <RosterContext.Provider 
-            value={{
-                selected: "russell wilson",
-                roster: ["dak prescott", 'pete carroll']
-            }}
-        >
+        <RosterContext.Provider value={{ selected, roster }} >
             {children}
         </RosterContext.Provider>
     );
